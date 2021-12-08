74be3c20a825a30c39459d39a10c9614876d7f9f
Szoták Simon <szotak.simon16@gmail.com>
2021-12-08 16:47:40 +0100

    KÉSZ

--- src/main/java/com/example/haromszogek/HelloController.java
+++ src/main/java/com/example/haromszogek/HelloController.java
@@ -5,12 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class HelloController {
@@ -19,6 +22,10 @@ public class HelloController {
     public ListView<String> listViewHibak;
     @FXML
     public ListView<DHaromszog> listViewDerekszog;
+    @FXML
+    public Label labelTerulet;
+    @FXML
+    public Label labelKerulet;
     @FXML
     private Button buttonAdatokBetoltese;
 
@@ -35,6 +42,11 @@ public class HelloController {
     }
 
     private void fajlBeolvas(String fajl){
+
+        listViewHibak.getItems().clear();
+        listViewDerekszog.getItems().clear();
+
+
         try{
             FileReader fr = new FileReader(fajl);
             BufferedReader br = new BufferedReader(fr);
@@ -44,6 +56,7 @@ public class HelloController {
             while(sor != null){
                 try{
                     DHaromszog dh = new DHaromszog(sor, i++);
+                    listViewDerekszog.getItems().add(dh);
                 }catch (Exception e){
                     listViewHibak.getItems().add(e.getMessage());
                 }finally {
@@ -58,5 +71,13 @@ public class HelloController {
         }catch (Exception e){
             System.err.println(e.getMessage());
         }
+
+    }
+
+    @FXML
+    private void onListViewClick(MouseEvent mouseEvent) {
+        DHaromszog dh = listViewDerekszog.getSelectionModel().getSelectedItem();
+        labelKerulet.setText(String.format("Kerület = %.2f",dh.getKerulet()));
+        labelTerulet.setText(String.format("Terület = %.2f",dh.getTerulet()));
     }
 }
